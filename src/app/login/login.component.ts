f53db9ee5551c858bafd3a294d76c8ab8faeb14b
anaya <az16001@ucad.edu.dv>
2024-10-13 04:36:43 -0600

    Validaciones en los campos de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  public formLogin: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formLogin = this.fb.group({
+      user: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  signIn() {
+    const condicion: boolean = this.formLogin.invalid;
+
+    if(condicion){
+      Object.values(this.formLogin.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }else{
+      console.log('Formulario válido');
+    }
+  }
+
+  public get formLoginControls(): any {
+    return this.formLogin.controls;
+  }
 }
